5d50e093962c2dd46887694c75ac05fee011262f
ANITA DJELASSI <mdjelassi@sunrise.ch>
2024-04-25 16:34:24 +0200

    FizzBuzzCode

---
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/FizzBuzz.java
@@ -0,0 +1,16 @@
+package ch.zhaw.iwi.devops.demo;
+
+public class FizzBuzz {
+
+     public static String calculate(int number) {
+        if (number % 15 == 0) {
+            return "FizzBuzz";
+        } else if (number % 3 == 0) {
+            return "Fizz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
+        } else {
+            return Integer.toString(number);
+        }
+    }
+}
