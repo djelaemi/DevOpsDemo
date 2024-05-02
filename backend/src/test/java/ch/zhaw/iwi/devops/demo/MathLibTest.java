1b127de14241dc3cb4e2250e58c3d2e2ec4cfd07
ANITA DJELASSI <mdjelassi@sunrise.ch>
2024-05-02 16:01:15 +0200

    MathLibChange

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/MathLibTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/MathLibTest.java
@@ -2,8 +2,6 @@ package ch.zhaw.iwi.devops.demo;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Answers.values;
-
 import org.junit.jupiter.api.Test;
 
 public class MathLibTest {
